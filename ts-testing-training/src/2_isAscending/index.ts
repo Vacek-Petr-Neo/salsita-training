21e45b2b9c6bf12b357d34aaf63bcf0a8ffd91d2
Thomas Morris <tmorris@salsitasoft.com>
2022-10-04 10:45:13 +0200

    Add docs for 2_isAscending

--- ts-testing-training/src/2_isAscending/index.ts
+++ ts-testing-training/src/2_isAscending/index.ts
@@ -1,1 +1,12 @@
-// See https://www.codewars.com/kata/56b7f2f3f18876033f000307 for prompt
+// Make a function receives an array of integers as input. Your task
+// is to determine whether the numbers are in ascending order. An array is said
+// to be in ascending order if there are no two adjacent integers where the left
+// integer exceeds the right integer in value.
+//
+// Example:
+// isAscending([]); // returns true
+// isAscending([9]); // returns true
+// isAscending([1, 1]); // returns true
+// isAscending([1, 2, 4, 7, 19]); // returns true
+// isAscending([1, 6, 2]); // returns false
+// isAscending([9, 8, 7]); // returns false
