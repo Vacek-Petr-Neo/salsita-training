d2d02e4d988b8cba71d140c6a02e3f75aef1e094
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-02 14:40:14 +0100

    0_numberStack - use beforeEach to make the test code shorter

--- ts-testing-training/src/1_numberStack/index.test.ts
+++ ts-testing-training/src/1_numberStack/index.test.ts
@@ -1,14 +1,16 @@
 import {NumberStack} from './index';
 
 describe('NumberStack', () => {
-  /* worked initially
+  let testSubject: NumberStack;
+  beforeEach(() => {
+    testSubject = new NumberStack();
+  });
+
   it('should not return a value after push call', () => {
     expect(testSubject.push(10)).toBe(undefined);
   });
-   */
 
   it('should return the last pushed value on pop() call', () => {
-    const testSubject = new NumberStack();
     testSubject.push(10);
     testSubject.push(5.5);
 
@@ -16,7 +18,6 @@ describe('NumberStack', () => {
   });
 
   it('should return the correct value after subsequent pop() calls', () => {
-    const testSubject = new NumberStack();
     testSubject.push(10);
     testSubject.push(5.5);
 
@@ -25,7 +26,6 @@ describe('NumberStack', () => {
   });
 
   it('should return the contents by accessing the contents property', () => {
-    const testSubject = new NumberStack();
     testSubject.push(10);
     testSubject.push(5.5);
     const result = testSubject.contents;
@@ -33,20 +33,16 @@ describe('NumberStack', () => {
   });
 
   it('should return the last inserted value on top', () => {
-    const testSubject = new NumberStack();
     testSubject.push(10);
     testSubject.push(5.5);
 
     expect(testSubject.top).toStrictEqual(5.5);
   });
 
-  /* Worked initially
   it('should return the same value after subsequent top() calls', () => {
-    const testSubject = new NumberStack();
     testSubject.push(10);
     testSubject.push(5.5);
 
     expect(testSubject.top).toStrictEqual(testSubject.top);
   });
-  */
 });
