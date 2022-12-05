8b42a7cb4d3359509e74886c2c80b4bcd8dd2fd1
Thomas Morris <tmorris@salsitasoft.com>
2022-12-05 11:18:14 +0100

    Update the "backend" logic

--- react-toy-app/src/data.ts
+++ react-toy-app/src/data.ts
@@ -2,15 +2,15 @@
  * Since we don't have a fully working backend, use this instead.
  */
 
-enum PokemonType {
+export enum PokemonType {
     grass = 'Grass',
     fire = 'Fire',
     water = 'Water',
     flying = 'Flying',
-    electric = 'Electric'
+    electric = 'Electric',
 }
 
-interface Pokemon {
+export interface Pokemon {
     id: number;
     name: string;
     level: number;
@@ -18,7 +18,11 @@ interface Pokemon {
     picture?: string;
 }
 
-export const pokemonList: Pokemon[] = [
+export type UnpersistedPokemon = Omit<Pokemon, 'id'>;
+
+// Note: this should remain private and should not be modified directly.  If you
+// need to manipulate this, use the exported functions in this file.
+let pokemonList: Pokemon[] = [
     {
         id: 1,
         name: 'Bulbasaur',
@@ -41,9 +45,46 @@ export const pokemonList: Pokemon[] = [
         picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
     },
     {
+        id: 8,
         name: 'Pikachu',
         level: 8,
         type: PokemonType.electric,
         picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
     },
 ]
+
+export const getPokemonList = async () => {
+    return pokemonList
+}
+
+export const updatePokemon = async (pokemonId: Pokemon['id'], newInfo: Pokemon): Promise<Pokemon> => {
+    if (pokemonList.find(pokemon => pokemon.id === pokemonId) === undefined) {
+        throw new Error('Pokemon not found');
+    }
+
+    pokemonList = pokemonList.map(pokemon => {
+        if (pokemon.id !== pokemonId) return pokemon
+        return newInfo
+    })
+
+    return newInfo;
+}
+
+export const deletePokemon = async (pokemonId: Pokemon['id']): Promise<void> => {
+    if (pokemonList.find(pokemon => pokemon.id === pokemonId) === undefined) {
+        throw new Error('Pokemon not found');
+    }
+
+    pokemonList = pokemonList.filter(pokemon => pokemon.id !== pokemonId)
+}
+
+export const createPokemon = async (newInfo: UnpersistedPokemon): Promise<Pokemon> => {
+    const newId = pokemonList[pokemonList.length - 1].id + 1
+    const newPokemon = { id: newId, ...newInfo }
+    pokemonList = [
+        ...pokemonList,
+        newPokemon
+    ]
+
+    return newPokemon;
+}
