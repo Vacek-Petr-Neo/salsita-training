e2c1c42622a4a3c52e75c76057725245c253d4da
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-12 17:40:23 +0100

    Pokemon toy app - enable routing

--- react-toy-app/src/App.tsx
+++ react-toy-app/src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import * as RouteList from './routes'
-import PokemonEdit from "./PokemonEdit";
-import PokemonList from "./PokemonList";
 
-//const router = createBrowserRouter(RouteList.routes);
+const router = createBrowserRouter(RouteList.routes);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Pokemon Tracker</h2>
-        <PokemonEdit />
-        <PokemonList />
+        <RouterProvider router={router} />
       </header>
     </div>
   );
--- react-toy-app/src/routes.tsx
+++ react-toy-app/src/routes.tsx
@@ -3,6 +3,7 @@ import PokemonList from "./PokemonList";
 
 export const PATH_EDIT = '/pokemon/:id/edit'
 export const PATH_LIST = '/'
+export const PATH_ADD = '/pokemon/new'
 export const routes = [
   {
     path: PATH_LIST,
@@ -11,5 +12,9 @@ export const routes = [
   {
     path: PATH_EDIT,
     element: <PokemonEdit />,
-  }
+  },
+  {
+    path: PATH_ADD,
+    element: <PokemonEdit />,
+  },
 ];
