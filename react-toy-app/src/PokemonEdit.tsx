5292bb038882f75a441fb25b18c0f54e7dfae563
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-12 17:46:13 +0100

    Pokemon toy app - Add/Edit action - form initialization + onChange handling

--- react-toy-app/src/PokemonEdit.tsx
+++ react-toy-app/src/PokemonEdit.tsx
@@ -1,34 +1,63 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, ChangeEventHandler, useEffect, useState} from 'react';
 import './App.css';
 import * as Backend from './data';
 import {Pokemon, PokemonType} from './data'
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import PokemonDetail from "./PokemonDetail";
+import * as RouteList from './routes'
 
-/*
-type PokemonFormData = {
-  id: Pokemon['id'] | undefined,
-  name: Pokemon['name'];
-  level: Pokemon['level'];
-  type: Pokemon['type'];
-  picture: Pokemon['picture'],
-}
-- */
 function PokemonEdit() {
-  const {pokemonId} = useParams();
-  const [formData, setFormData]: [formData: Pokemon, setFormData: Function] = useState({
+  const navigate = useNavigate();
+  const paramsRaw = useParams();
+
+  const [formData, setFormData] = useState<Pokemon>({
     id: -1,
     name: '',
     picture: '',
     type: PokemonType.electric,
     level: 1,
   });
+
   const handleMount = () => {(async () => {
-    if (typeof pokemonId !== 'number') return;
+    if (paramsRaw.id === undefined) return;
+    const pokemonId: Pokemon['id'] | undefined = parseInt(paramsRaw.id);
+    if (Number.isNaN(pokemonId) || pokemonId < 1) return;
+
     const pokemonList : Pokemon[] = await Backend.getPokemonList();
-    //pokemonList.find()
+    const pokemon = pokemonList.find(pokemon => pokemon.id === pokemonId)
+    if (pokemon === undefined) {
+      navigate(RouteList.PATH_LIST);
+      return;
+    }
+    setFormData(pokemon);
   })()};
   useEffect(handleMount,[]);
+
+  const handleChange = (pokemonPropName: keyof Pokemon) : ChangeEventHandler<HTMLInputElement | HTMLSelectElement> => {
+    return (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      const newFormData = {...formData};
+      let newValue;
+      switch (pokemonPropName) {
+        case "name":
+        case "picture":
+          newFormData[pokemonPropName] = event.target.value;
+          break;
+        case "level":
+          newValue = parseInt(event.target.value);
+          if (Number.isNaN(newValue) || newValue < 1) return;
+          newFormData[pokemonPropName] = newValue;
+          break;
+        case "type":
+          newValue = Object.values(PokemonType).find(value => value === event.target.value);
+          if (newValue === undefined) return;
+          newFormData[pokemonPropName] = newValue;
+          break;
+        default:
+          return;
+      }
+      setFormData(newFormData);
+    }
+  };
 
   return (
     <main>
@@ -40,7 +69,10 @@ function PokemonEdit() {
                 <label htmlFor='pokemon-name'>Name</label>
               </td>
               <td>
-                <input name='pokemon-name' />
+                <input name='pokemon-name'
+                       value={formData.name}
+                       onChange={handleChange('name')}
+                />
               </td>
             </tr>
 
@@ -49,7 +81,11 @@ function PokemonEdit() {
                 <label htmlFor='pokemon-level'>Level</label>
               </td>
               <td>
-                <input name='pokemon-level' type='number' />
+                <input name='pokemon-level'
+                       type='number'
+                       value={formData.level}
+                       onChange={handleChange('level')}
+                />
               </td>
             </tr>
 
@@ -58,12 +94,12 @@ function PokemonEdit() {
                 <label htmlFor='pokemon-type'>Type</label>
               </td>
               <td>
-                <select name='pokemon-type'>
+                <select name='pokemon-type'
+                        value={formData.type}
+                        onChange={handleChange('type')}
+                >
                   {Object.entries(Backend.PokemonType).map(([typeKey, type]) =>
-                    typeKey === formData.type ?
-                      <option key={typeKey} selected>{type}</option>
-                    :
-                      <option key={typeKey}>{type}</option>
+                        <option key={typeKey}>{type}</option>
                   )}
                 </select>
               </td>
@@ -74,7 +110,11 @@ function PokemonEdit() {
                 <label htmlFor='pokemon-picture'>Image Url</label>
               </td>
               <td>
-                <input name='pokemon-picture' type='url' value={formData.picture}/>
+                <input name='pokemon-picture'
+                       type='url'
+                       value={formData.picture}
+                       onChange={handleChange('picture')}
+                />
               </td>
             </tr>
 
