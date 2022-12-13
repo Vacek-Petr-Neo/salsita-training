a4ef4d76af42b93bf4aee65d30060c06a82e054b
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-13 10:44:14 +0100

    3_wordLengthFilter - test and implementation

--- ts-testing-training/src/3_wordLengthFilter/index.test.ts
+++ ts-testing-training/src/3_wordLengthFilter/index.test.ts
@@ -1,5 +1,40 @@
+import {wordLengthFilter} from './index';
+
 describe('wordLengthFilter', () => {
-  it('should do stuff', () => {
-    // put stuff here
+  it('should work for empty string', () => {
+    expect(wordLengthFilter('', 4)).toHaveLength(0);
+  });
+  it('should return empty array for a massive maxLen param', () => {
+    const result = wordLengthFilter(
+        'The quick brown fox jumps over the lazy dog',
+        Number.MAX_SAFE_INTEGER,
+    );
+    expect(result).toHaveLength(0);
+  });
+  it('should work for a simple example', () => {
+    const result = wordLengthFilter(
+        'The quick brown fox jumps over the lazy dog',
+        4,
+    );
+    expect(result).toHaveLength(3);
+    expect(result).toContain('quick');
+    expect(result).toContain('brown');
+    expect(result).toContain('jumps');
+  });
+  it('should return array for all words for maxLen 0', () => {
+    const result = wordLengthFilter(
+        'The quick brown fox jumps over the lazy dog',
+        0,
+    );
+    expect(result).toHaveLength(9);
+    expect(result).toContain('The');
+    expect(result).toContain('quick');
+    expect(result).toContain('brown');
+    expect(result).toContain('fox');
+    expect(result).toContain('jumps');
+    expect(result).toContain('over');
+    expect(result).toContain('the');
+    expect(result).toContain('lazy');
+    expect(result).toContain('dog');
   });
 });
--- ts-testing-training/src/3_wordLengthFilter/index.ts
+++ ts-testing-training/src/3_wordLengthFilter/index.ts
@@ -3,3 +3,8 @@
 // returns a list of all words that are longer than n.
 //
 // https://www.codewars.com/kata/5697fb83f41965761f000052
+export const wordLengthFilter = (input: string, maxLen: number) : string[] => {
+  return input.split(' ').filter(
+      (value: string) => value.length > maxLen,
+  );
+};
