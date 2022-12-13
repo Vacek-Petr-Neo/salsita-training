659e0215219cea7bce8d2975167a5923357bdac4
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-13 13:22:52 +0100

    4_isPrime - test and implementation

--- ts-testing-training/src/4_isPrime/index.test.ts
+++ ts-testing-training/src/4_isPrime/index.test.ts
@@ -1,5 +1,38 @@
+import {isPrime} from './index';
+
 describe('isPrime', () => {
-  it('should return true if passed 0', () => {
-    // make me!
+  it('should return false if passed -10', () => {
+    expect(isPrime(-10)).toStrictEqual(false);
+  });
+  it('should return false if passed 0', () => {
+    expect(isPrime(0)).toStrictEqual(false);
+  });
+  it('should return false if passed 1', () => {
+    expect(isPrime(1)).toStrictEqual(false);
+  });
+  it('should return false for non-integer like PI', () => {
+    expect(isPrime(Math.PI)).toStrictEqual(false);
+  });
+  it('should return true if passed 2', () => {
+    expect(isPrime(2)).toStrictEqual(true);
+  });
+  it('should return true if passed 3', () => {
+    expect(isPrime(3)).toStrictEqual(true);
+  });
+  it('should return false if passed 4', () => {
+    expect(isPrime(4)).toStrictEqual(false);
+  });
+  it('should return false if passed 8', () => {
+    expect(isPrime(8)).toStrictEqual(false);
+  });
+  it('should return false for NaN', () => {
+    expect(isPrime(NaN)).toStrictEqual(false);
+  });
+  it('should return false for infinity', () => {
+    expect(isPrime(Infinity)).toStrictEqual(false);
+  });
+  it('should return false for -infinity', () => {
+    console.log(-Infinity);
+    expect(isPrime(-Infinity)).toStrictEqual(false);
   });
 });
--- ts-testing-training/src/4_isPrime/index.ts
+++ ts-testing-training/src/4_isPrime/index.ts
@@ -3,5 +3,10 @@
 // if it's not.  It should return false for 0.
 
 export const isPrime = (input: number): boolean => {
-  return false;
+  if (input < 2 || !Number.isInteger(input)) return false;
+  const boundary = input / 2;
+  for (let i = 2; i <= boundary; i++) {
+    if (input % i === 0) return false;
+  }
+  return true;
 };
