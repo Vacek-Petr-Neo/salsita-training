026885ab6ab553a0080cf6f24df6e23183935abe
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-15 20:53:27 +0100

    5_dumbPhone - test and implementation

--- ts-testing-training/src/5_dumbPhone/index.test.ts
+++ ts-testing-training/src/5_dumbPhone/index.test.ts
@@ -1,5 +1,16 @@
+import {getKeystrokes} from './index';
+
 describe('getKeystrokes', () => {
-  it('should do stuff', () => {
-    // put stuff here
+  it('should return 4433555p555666096667775553 for "HELLO WORLD"', () => {
+    expect(getKeystrokes('HELLO WORLD'))
+        .toStrictEqual('4433555p555666096667775553');
+  });
+  it('should return 4433555p555666096667775553 for "hello world"', () => {
+    expect(getKeystrokes('hello world'))
+        .toStrictEqual('4433555p555666096667775553');
+  });
+  it('should return 4433555p555666096667775553 for "hello world"', () => {
+    expect(getKeystrokes('H3770 W0R7D'))
+        .toStrictEqual('44333377777p7777700p0900777p777773');
   });
 });
