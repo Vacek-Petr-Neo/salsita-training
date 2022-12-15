4432bc806c074c022b8b11ad1cbbf63a14256e1b
Petr Vacek <vacekpetrneo@gmail.com>
2022-12-15 20:53:27 +0100

    5_dumbPhone - test and implementation

--- ts-testing-training/src/5_dumbPhone/index.ts
+++ ts-testing-training/src/5_dumbPhone/index.ts
@@ -43,3 +43,59 @@
 //
 // Try avoiding the obvious a-z combinations in order to support ease in change
 // of Keypad combinations
+
+const map = new Map<string, string>([
+  ['1', '1'],
+  ['A', '2'],
+  ['B', '22'],
+  ['C', '222'],
+  ['2', '2222'],
+  ['D', '3'],
+  ['E', '33'],
+  ['F', '333'],
+  ['3', '3333'],
+  ['G', '4'],
+  ['H', '44'],
+  ['I', '444'],
+  ['4', '4444'],
+  ['J', '5'],
+  ['K', '55'],
+  ['L', '555'],
+  ['5', '5555'],
+  ['M', '6'],
+  ['N', '66'],
+  ['O', '666'],
+  ['6', '6666'],
+  ['P', '7'],
+  ['Q', '77'],
+  ['R', '777'],
+  ['S', '7777'],
+  ['7', '77777'],
+  ['T', '8'],
+  ['U', '88'],
+  ['V', '888'],
+  ['8', '8888'],
+  ['W', '9'],
+  ['X', '99'],
+  ['Y', '999'],
+  ['Z', '9999'],
+  ['9', '99999'],
+  ['*', '*'],
+  [' ', '0'],
+  ['0', '00'],
+  ['#', '#'],
+]);
+
+export const getKeystrokes = (input: string) : string => {
+  input = input.toUpperCase();
+  const outBuffer : string[] = [map.get(input[0]) ?? ''];
+  let image : string | undefined;
+  for (let i = 1; i < input.length; i++) {
+    image = map.get(input[i]) ?? '';
+    if (image[0] === outBuffer[outBuffer.length-1][0]) {
+      outBuffer.push('p');
+    }
+    outBuffer.push(image);
+  }
+  return outBuffer.join('');
+};
